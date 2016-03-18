63fb2066334c201b7c824b20abb769ab7f0732eb
hdelattre <hdelattre@berkeley.edu>
2016-03-18 05:50:27 +0000

    fixed step def conflicts

--- features/step_definitions/admin_panel_steps.rb
+++ features/step_definitions/admin_panel_steps.rb
@@ -67,10 +67,6 @@ Then(/^the details of "([^"]*)" should be hidden$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I press "([^"]*)" on "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 Then(/^the details of "([^"]*)" should not be hidden$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
