f7035d16d0eefece6c8017aa3fede3d273c3359f
Mitchell Oleson <moleson21@berkeley.edu>
2016-04-20 21:36:08 +0000

    somethings wrong with admin panel

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -37,9 +37,7 @@ class SyncsController < ApplicationController
       events = calendar_pull(params[:sync][:url])
       if events.respond_to?(:each)
         events.each do |event|
-          e = Event.new(event)
-          e.update_attributes(:status => 'pending')
-          e.save!
+          Event.new(event)
         end
       end
       @sync.update_attributes(:organization => @name.gsub("-", " "), :last_sync => DateTime.now())
